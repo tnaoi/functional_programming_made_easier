242053b4b6f29588c6314b276622d0bcd308bae6
Taro Naoi <taronaoi86@gmail.com>
2023-02-22 11:10:24 -0800

    add null and singletone

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -1,8 +1,20 @@
-module Ch5 where
+module Ch5
+  ( (#)
+  , apply
+  , applyFlipped
+  , const
+  , flip
+  , null
+  , singleton
+  , test
+  )
+  where
 
+import Data.List (List(..), (:))
 import Effect (Effect)
 import Effect.Console (log)
 import Prelude (Unit, show, discard)
+import Prim.RowList (Nil)
 
 -- flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
 -- flip f x y = f y x
@@ -23,10 +35,20 @@ apply f x = f x
 applyFlipped :: ∀ a b. a -> (a -> b) -> b
 applyFlipped = flip apply
 
+singleton :: ∀ a. a -> List a
+singleton x = x : Nil
+
+null :: ∀ a. List a -> Boolean
+null Nil = true
+null _   = false
+
 infixr 0 apply as $
 infixl 1 applyFlipped as #
 
 test :: Effect Unit
 test = do
-  log $ show $ flip const 1 2
-  flip const 1 2 # show # log
+  -- log $ show $ flip const 1 2
+  -- flip const 1 2 # show # log
+  log $ show $ singleton "xyz"
+  log $ show $ null Nil
+  log $ show $ null ("abc" : Nil)
