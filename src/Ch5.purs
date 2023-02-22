9e52d93e110524526d87bbc348524c38a7ce5a68
Taro Naoi <taronaoi86@gmail.com>
2023-02-21 17:59:12 -0800

    add flipped, const, #, $ operators

---
+++ src/Ch5.purs
@@ -0,0 +1,32 @@
+module Ch5 where
+
+import Effect (Effect)
+import Effect.Console (log)
+import Prelude (Unit, show, discard)
+
+-- flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
+-- flip f x y = f y x
+
+-- -- Alternative version of flip with lambda.
+-- flip :: ∀ a b c. (a -> b -> c) -> (b -> a -> c)
+-- flip f = \x y -> f y x
+
+flip :: ∀ a b c. (a -> b -> c) -> b -> (a -> c)
+flip f x = \y -> f y x
+
+const :: ∀ a b. a -> b -> a
+const x _ = x 
+
+apply :: ∀ a b. (a -> b) -> a -> b
+apply f x = f x
+
+applyFlipped :: ∀ a b. a -> (a -> b) -> b
+applyFlipped = flip apply
+
+infixr 0 apply as $
+infixl 1 applyFlipped as #
+
+test :: Effect Unit
+test = do
+  log $ show $ flip const 1 2
+  flip const 1 2 # show # log
