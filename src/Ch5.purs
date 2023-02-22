5856bf88162708ab0c3c0b6d46b7396009a6f348
Taro Naoi <taronaoi86@gmail.com>
2023-02-22 14:54:22 -0800

    add more functions

--- src/Ch5.purs
+++ src/Ch5.purs
@@ -4,16 +4,21 @@ module Ch5
   , applyFlipped
   , const
   , flip
+  , head
+  , length
   , null
   , singleton
+  , snoc
+  , tail
   , test
   )
   where
 
 import Data.List (List(..), (:))
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, show, discard)
+import Prelude (Unit, (+), (==), show, discard)
 import Prim.RowList (Nil)
 
 -- flip :: ∀ a b c. (a -> b -> c) -> b -> a -> c
@@ -42,6 +47,34 @@ null :: ∀ a. List a -> Boolean
 null Nil = true
 null _   = false
 
+snoc :: ∀ a. List a -> a -> List a
+snoc Nil x = singleton x
+snoc (y : ys) x = y : snoc ys x
+
+-- -- Not tail recursive.
+-- length :: ∀ a. List a -> Int
+-- length Nil = 1
+-- length (_ : ys) = length ys
+
+-- Tail recursive version of length using private go function.
+length :: ∀ a. List a -> Int
+length l = go 0 l where
+  go :: Int -> List a -> Int
+  go acc Nil      = acc
+  go acc (_ : xs) = go (1 + acc) xs
+
+head :: ∀ a. List a -> Maybe a
+head Nil = Nothing
+head (x : _) = Just x
+
+tail :: ∀ a. List a -> Maybe (List a)
+tail Nil      = Nothing
+tail (_ : xs) = Just xs
+
+last :: ∀ a. List a -> Maybe a
+last Nil    = Nothing
+last (_ : xs) = if length xs == 1 then head xs else last xs
+
 infixr 0 apply as $
 infixl 1 applyFlipped as #
 
@@ -49,6 +82,14 @@ test :: Effect Unit
 test = do
   -- log $ show $ flip const 1 2
   -- flip const 1 2 # show # log
-  log $ show $ singleton "xyz"
-  log $ show $ null Nil
-  log $ show $ null ("abc" : Nil)
+  -- log $ show $ singleton "xyz"
+  -- log $ show $ null Nil
+  -- log $ show $ null ("abc" : Nil)
+  -- log $ show $ snoc (1 : 2 : Nil) 3
+  -- log $ show $ length $ 1 : 2 : 3 : Nil
+  -- log $ show $ head (Nil :: List Unit)
+  -- log $ show $ head ("abc" : "123" : Nil)
+  -- log $ show $ tail (Nil :: List Unit)
+  -- log $ show $ tail ("abc" : "123" : Nil)
+  log $ show $ (last Nil :: Maybe Unit)
+  log $ show $ last ("a" : "b" : "c" : Nil)
